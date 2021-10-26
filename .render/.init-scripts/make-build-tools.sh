78b427cbd94c4f17cc0125476c3814cbd34c7de7
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-10-26 17:32:16 +0100

    Use deploy action branch of tools

--- .render/.init-scripts/make-build-tools.sh
+++ .render/.init-scripts/make-build-tools.sh
@@ -2,5 +2,5 @@
 
 set -o errexit
 
-git clone --single-branch --branch main https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
+git clone --single-branch --branch deploy-action https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
 .scripts/install-dependencies.sh
